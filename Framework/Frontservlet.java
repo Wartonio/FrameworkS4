6804ca2d20a0c1cb6173d872e326e1a01c0dbf8b
Wartonio <120163694+Wartonio@users.noreply.github.com>
2023-05-05 08:58:45 +1200

    Add files via upload

--- Framework/Frontservlet.java
+++ Framework/Frontservlet.java
@@ -1,12 +1,47 @@
 package etu001935.framework.servlet;
 import etu001935.framework.*;
+import etu001935.framework.annotation.*;
 import jakarta.servlet.*;
 import java.io.*;
 import jakarta.servlet.http.*;
 import java.util.*;
+import java.net.*;
+import java.lang.reflect.*;
 
 public class Frontservlet extends HttpServlet{
     HashMap<String,Mapping> MappingUrls;
+
+    public void init() throws ServletException{
+        try {
+            super.init();
+            // String packages = String.valueOf(getInitParameter("packages"));
+            String packages = "etu001935.model";
+            this.MappingUrls=new HashMap<>();
+            String path = packages.replaceAll("[.]","\\\\");
+            URL packageUrl=Thread.currentThread().getContextClassLoader().getResource(path);
+            File packDir =new File(packageUrl.toURI());
+            File[] inside=packDir.listFiles(file->file.getName().endsWith(".class"));
+            List<Class> lists = new ArrayList<>();
+            for(File f : inside){
+                    String c =packages+"."+f.getName().substring(0,f.getName().lastIndexOf("."));
+                    lists.add(Class.forName(c));
+            }
+            for(Class c:lists){
+                Method[] methods = c.getDeclaredMethods();
+                for(Method m : methods){
+                    if(m.isAnnotationPresent(Annotation.class)){
+                        Annotation url = m.getAnnotation(Annotation.class);
+                        if(!url.Url().isEmpty() && url.Url() !=null){
+                            Mapping map = new Mapping(c.getName() , m.getName());
+                            this.MappingUrls.put(url.Url(),map);
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
 
     public void processRequest(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
 
