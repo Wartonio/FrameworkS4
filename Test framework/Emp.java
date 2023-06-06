1b030df643903db7327085560544e0de9ec58e05
Wartonio <antonioandria0803@gmail.com>
2023-06-06 19:30:35 +1200

    Fanampiny

--- Test framework/Emp.java
+++ Test framework/Emp.java
@@ -1,9 +1,13 @@
 package etu001935.model;
 import etu001935.framework.annotation.*;
 import etu001935.modelView.ModelView;
+import java.sql.Date;
 public class Emp {
+    Integer id;
     String nom;
-    
+    Date date;
+    String[] jours;
+
     @Annotation(Url="Getall")
     public ModelView GetAll(){
        ModelView view = new ModelView("essaie.jsp");
@@ -13,10 +17,27 @@ public class Emp {
         view.AddItem("all_emp",emps);
         return view;
     }
+    public String[] getJours(){
+        return jours;
+    }
+    public void setJours(String[] jours){
+        this.jours=jours;
+    }
     public String getNom() {
         return nom;
     }
-
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    public Integer  getId(){
+        return id;
+    }
+    public void setId(Integer id){
+        this.id=id;
+    }
     public Emp(){}
 
     public void setNom(String newname) {
@@ -24,8 +45,5 @@ public class Emp {
     }
     public Emp(String name) {
         setNom(name);
-
-
-
     }
 }
